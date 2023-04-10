47c11536c1a3a91f88a502eb54901f75b248e8cc
moreira-rodrigo <92392263+moreira-rodrigo@users.noreply.github.com>
2023-04-10 12:19:06 -0300

    06

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import os
 def generate_event(event_name):
-    path = 'athletics_events/'+ str(event_name) 
+    newpath = 'athletics_events/'+ str(event_name) 
     if not os.path.exists(newpath):
         os.makedirs(newpath)
     return true
