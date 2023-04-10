aaf83a2273872df4a5215e90c085762131af787d
moreira-rodrigo <92392263+moreira-rodrigo@users.noreply.github.com>
2023-04-10 11:54:37 -0300

    ident_edit

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 def generate_event(event_name):
     path = 'athletics_events/'+event_name 
     if not os.path.exists(newpath):
-    os.makedirs(newpath)
+        os.makedirs(newpath)
     return true
